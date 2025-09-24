fbc413209abf1a830d8451a1b1c8c2a39c71bd8c
Ooozari <uzairasiff1227@gmail.com>
2025-09-24 11:38:40 +0500

    Lab 2: Fixed the path of dataset and print the dataframe

--- Lab2/dataSet/preprocessing.py
+++ Lab2/dataSet/preprocessing.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("/data")
+df = pd.read_csv("./dataSet/Admission_Predict.csv")
 
 columns=df.columns
 df.drop("Serial No.", axis=1, inplace=True)
 y=df["Chance of Admit "]
 df.drop("Chance of Admit ", axis=1, inplace=True)
-df.head()
+print(df.head())
+
