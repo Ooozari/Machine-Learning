15101f1949860a1f32d90341503fbb30e432c7b3
Ooozari <uzairasiff1227@gmail.com>
2025-09-24 11:44:07 +0500

    Lab 2: add train_test_split and Linear Regression model training with predictions

--- Lab2/dataSet/preprocessing.py
+++ Lab2/dataSet/preprocessing.py
@@ -8,3 +8,14 @@ y=df["Chance of Admit "]
 df.drop("Chance of Admit ", axis=1, inplace=True)
 print(df.head())
 
+# Implementing the train_test_split
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(df,y,test_size=0.2)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(x_train,y_train)
+pred = lr.predict(x_test)
+print(pred)
